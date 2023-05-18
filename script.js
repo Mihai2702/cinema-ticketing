9bcc61cb8f1ee82384e1254e757589dc2dd3d002
Velicu Mihai <mitzzza.nr1@gmail.com>
2023-05-18 17:25:51 +0300

    add fetch and switch for selecting movie

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-const seatingArrangement = [
-  [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 1],
-  [0, 0, 0, 2, 1, 1, 0, 0, 2, 0, 0, 0],
-  [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 1],
-  [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 1],
-  [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 1, 1],
-  [0, 1, 0, 2, 0, 0, 0, 0, 2, 0, 1, 1],
-];
-
+let price;
 const container = document.querySelector('.container');
-const seats = [];
 
-// Generate seats based on the arrangement array
-seatingArrangement.forEach((row, rowIndex) => {
-  const seatRow = document.createElement('div');
-  seatRow.classList.add('row');
-
-  row.forEach((seatType, seatIndex) => {
-    const seat = document.createElement('div');
-    seat.classList.add('seat');
-    seat.classList.add(`seat-${seatIndex + 1}`);
-
-    if (seatType === 1) {
-      seat.classList.add('occupied');
-    } else if (seatType === 2) {
-      seat.classList.add('corridor');
-    }
-
-    seat.addEventListener('click', () => {
-      seat.classList.toggle('selected');
-      updateSelectedCount();
-      calculateTotalPrice();
-    });
-
-    seatRow.appendChild(seat);
-    seats.push(seat);
-  });
-
-  container.appendChild(seatRow);
-});
-
+// Update the count number of the seats
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.seat.selected');
   const selectedSeatsCount = selectedSeats.length - 1;
@@ -51,11 +14,74 @@ const calculateTotalPrice = () => {
   const selectedSeats = document.querySelectorAll('.seat.selected');
   const selectedSeatsCount = selectedSeats.length - 1;
 
-  const ticketPrice = movie.value;
+  const ticketPrice = price;
   const totalPrice = selectedSeatsCount * ticketPrice;
 
   document.querySelector('#total').innerText = totalPrice;
 };
 
-updateSelectedCount();
-calculateTotalPrice();
+const fetchJsonData = url => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const seatingArrangement = data.arrangement;
+      price = data.price;
+      generateSeats(seatingArrangement);
+      calculateTotalPrice();
+    });
+};
+
+const handleMovieSelection = () => {
+  const movieSelect = document.getElementById('movie');
+  const selectedMovieValue = movieSelect.value;
+  let url;
+
+  switch (selectedMovieValue) {
+    case '1':
+      url = 'proiectii/1.json';
+      break;
+    case '2':
+      url = 'proiectii/2.json';
+      break;
+    case '3':
+      url = 'proiectii/3.json';
+      break;
+  }
+  fetchJsonData(url);
+};
+
+const generateSeats = seatingArrangement => {
+  container.innerHTML = ''; // Clear existing seats
+
+  seatingArrangement.forEach((row, rowIndex) => {
+    const seatRow = document.createElement('div');
+    seatRow.classList.add('row');
+
+    row.forEach((seatType, seatIndex) => {
+      const seat = document.createElement('div');
+      seat.classList.add('seat');
+      seat.classList.add(`seat-${seatIndex + 1}`);
+
+      if (seatType === 1) {
+        seat.classList.add('occupied');
+      } else if (seatType === 2) {
+        seat.classList.add('corridor');
+      }
+      if (seatType === 0) {
+        seat.addEventListener('click', () => {
+          seat.classList.toggle('selected');
+          updateSelectedCount();
+          calculateTotalPrice();
+        });
+      }
+      seatRow.appendChild(seat);
+    });
+
+    container.appendChild(seatRow);
+  });
+};
+
+const movieSelect = document.getElementById('movie');
+movieSelect.addEventListener('change', handleMovieSelection);
+
+handleMovieSelection();
